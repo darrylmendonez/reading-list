c58929acc7bbdfb96e2b4f08baa37c882a0929af
Darryl Mendonez <darryl.mendonez@nucleuscentral.com>
2019-04-04 18:05:52 -0400

    created mlab db and connected to app

--- server/app.js
+++ server/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+const dbUrl = 'mongodb://darryl:test123@ds231956.mlab.com:31956/gql-reading-list';
 
 const app = express();
+
+// connect to mlab database
+mongoose.connect(dbUrl, { useNewUrlParser: true });
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+});
 
 app.use('/graphql', graphqlHTTP({
   schema,
